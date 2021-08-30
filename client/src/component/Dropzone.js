ab5272176dc0543c73bdf689b23b269e87fbf97f
qijieguan <qijieguan7@gmail.com>
2021-08-30 02:25:43 -0700

    created a dropzone and preview for image

---
+++ client/src/component/Dropzone.js
@@ -0,0 +1,28 @@
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone';
+
+export default function MyDropzone( {getFiles} ) {
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        getFiles(acceptedFiles);
+    })
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        onDrop,
+        accepts: "images/*",
+        multiple: false,
+    });
+    
+    return(
+        <div {...getRootProps()} className="Dropzone" style={isDragActive ? activeStyle : {}}>
+            <input {...getInputProps()} />
+            Upload image here
+        </div>
+    );
+}
+
+const activeStyle = {
+    borderStyle: "solid",
+    opacity: "0.5"
+}
--- client/src/component/Item.js
+++ client/src/component/Item.js
@@ -1,7 +1,18 @@
 
 const Item = ({item}) => {
     return(
-        <div className="Item">{item.name}</div>
+        <div className="Item-Container">
+            <div className="Item-Img"></div>
+            <div className="Item-Details">
+                <div className="Item-Name">
+                    {item.name}
+                </div>
+                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                    <div className="Item-Price">Price: ${(item.price).toFixed(2)}</div>
+                    <div className="Item-Stock">Stock Left: {item.stock}</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
