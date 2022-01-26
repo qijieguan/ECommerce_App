364de78e9f8678ff567dee8dff5142c3e1b32259
qijieguan <qijieguan7@gmail.com>
2022-01-25 23:31:14 -0800

    finalized project

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -1,7 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from 'react';
-import { FaTrashAlt } from 'react-icons/fa'
+import { FaTrashAlt } from 'react-icons/fa';
+import { BsCheckCircle } from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { deleteCart, clearCart } from './actions/index.js';
 
 
@@ -12,6 +14,7 @@ const Checkout = () => {
 
     const [cost, setCost] = useState(useLocation().state.cost);
     const [message, setMessage] = useState("Cart is Empty. Happy shopping!");
+    const [paid, setPaid] = useState(false);
 
     useEffect(() => {
         if (cart.length === 0) {setCost(0); return;} 
@@ -31,15 +34,18 @@ const Checkout = () => {
 
     const handlePay = () => {
         setMessage("Thank you for shopping! Happy New Year!");
-        dispatch(clearCart())
-        document.getElementsByClassName("Checkout-Msg")[0].style.color = "green";
+        dispatch(clearCart());
+        setPaid(true);
+        document.getElementsByClassName("Checkout-Msg")[0].firstChild.style.color = "green";
     };
 
     return (
         <div style={{display: 'flex', marginTop: '100px'}}>
-            <div className="Checkout-Msg" 
-                style={{display: cart.length === 0 ? 'flex' : 'none', fontSize: '40px', color: "gray"}}
-            >{message}</div>
+            <div className="Checkout-Msg" style={{display: cart.length === 0 ? 'flex' : 'none', flexDirection: 'column'}}>   
+                <div style={{margin: '40px 0', fontSize: '50px', color: "gray"}}>{message}</div>
+                <AiOutlineShoppingCart size={225} color='gray' style={{display: paid ? 'none' : 'block'}}/>
+                <BsCheckCircle size={225} color='green' style={{display: paid ? 'block' : 'none'}}/>
+            </div>
             <div style={{display: cart.length > 0 ? 'flex' : 'none', width: '80%'}}>
                 <div className='Cart-Items'>
                     {cart.map(item => 
