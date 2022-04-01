73114a0ec4a6d48fecaceee0dcbdc9db79974fce
qijieguan <qijieguan7@gmail.com>
2022-04-01 14:40:14 -0700

    changed last slide on front page

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,28 +19,31 @@ export default function Home() {
                 </h1>
             </div>
             <div id="home-content">
-                <div className='slide-side from-left'>
-                    <h1>Values Of Furnitures</h1>
-                    <span>
-                        Your Living Space Can Influence Your <span> Emotion</span> And 
-                        <span> Mind</span> On A Daily Basis. Furnish Your Home To Fulfill Your 
-                        <span> Inner Satisfaction</span>. Furnitures Can Create 
-                        Your <span>Ideal Atmosphere </span> And Give You A Peace Of Mind.
-                    </span>
+                <h1>More About Us</h1>
+                <div id="slide-card">
+                    <div className='slide-side from-left'>
+                        <h1>Values Of Furnitures</h1>
+                        <span>
+                            Your Living Space Can Influence Your <span> Emotion</span> And 
+                            <span> Mind</span> On A Daily Basis. Furnish Your Home To Fulfill Your 
+                            <span> Inner Satisfaction</span>. Furnitures Can Create 
+                            Your <span>Ideal Atmosphere </span> And Give You A <span>Peace </span>Of Mind.
+                        </span>
+                    </div>
+                    <div className='slide-side from-left'>
+                        <h1>Become A Client Today</h1>
+                        <span>
+                            We Offer A Large Variety Of Furnitures <span> In-Store and Online</span>. 
+                            Most Of Our Furnitures Are Imported <span> From Foreign Companies</span>.
+                            What You Can Find Are Furnitures With <span>Unique Designs </span> 
+                            That Draw Curiosity And Attention. We Also Have Other Products In-Store.
+                        </span>
+                    </div>
                 </div>
-                <div className='slide-side from-right-1'/>
-            </div>
-            <div id="home-content" style={{borderTop: '0'}}>
-                <div className='slide-side from-left'>
-                    <h1>Become A Client Today</h1>
-                    <span>
-                        We Offer A Large Variety Of Furnitures <span> In-Store and Online</span>. 
-                        Most Of Our Furnitures Are Imported <span> From Foreign Companies</span>.
-                        What You Can Find Are Furnitures With <span>Unique Designs </span> 
-                        That Draw Curiosity And Attention. We Also Have Other Products In-Store.
-                    </span>
+                <div id="slide-image">
+                    <div className='slide-side from-right-1'/>
+                    <div className='slide-side from-right-2'/>
                 </div>
-                <div className='slide-side from-right-2'/>
             </div>
         </div>
     )
